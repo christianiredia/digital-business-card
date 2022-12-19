b6c431e8013df1df12c3e1ee26b96ff4c1c7c32d
Christian Iredia <101851819+christianiredia@users.noreply.github.com>
2022-12-19 01:10:09 -0600

    Added Buttons and layout styling

--- business-card/src/Info.jsx
+++ business-card/src/Info.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Headshot from "./images/HeadShot.jpg";
 import "./info.css";
 import Email from "./images/email.png";
-import LinkedIn from "./images/linkedin.svg";
+import LinkedIn from "./images/linkedin.png";
 
 const Info = () => {
   return (
@@ -12,17 +12,24 @@ const Info = () => {
         className="headshot"
         alt="Picture of Christian Iredia"
       />
-      <h1>Christian Iredia</h1>
-      <p>Frontend Developer</p>
-      <a href="https://github.com/christianiredia">christian iredia website</a>
-      <button>
-        <img src={Email} className="email" alt="Mail" />
-        Email
-      </button>
-      <button>
-        <img src={LinkedIn} className="linkedin" alt="Linkedin Logo" />
-        Linkedin
-      </button>
+      <>
+        <h1>Christian Iredia</h1>
+        <p>Frontend Developer</p>
+        <a href="https://github.com/christianiredia">christianiredia.website</a>
+      </>
+
+      <div className="btnContainer">
+        <button className="emailBtn">
+          <>
+            <img src={Email} className="email" alt="Mail" />
+          </>
+          <>Email</>
+        </button>
+        <button className="linkedinBtn">
+          <img src={LinkedIn} className="linkedin" alt="Linkedin Logo" />
+          Linkedin
+        </button>
+      </div>
     </header>
   );
 };
